ed10e340b1d6ab13fdb6a92ab56084d8837f01ab
Jesus Antonio Arellano Zapata <tonyza0902@hotmail.com>
2026-08-26 12:43:13 -0600

    fix: Cambio de IP en Config para pruebas con móvil

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: false, // html5-qrcode no es compatible con el doble mount de Strict Mode
-  allowedDevOrigins: ['192.168.8.*', 'localhost:3000'],
+  allowedDevOrigins: ['192.168.8.*', '192.168.1.*', 'localhost:3000'],
 };
 
 export default nextConfig;
